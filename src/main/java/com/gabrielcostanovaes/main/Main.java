463eb34f87119207ce2fc8af196dfa598615db5c
Gabriel Henrique <gabidelnovaes@gmail.com>
2025-02-06 16:46:33 -0300

    Completing main class API

--- src/main/java/com/gabrielcostanovaes/main/Main.java
+++ src/main/java/com/gabrielcostanovaes/main/Main.java
@@ -1,4 +1,18 @@
 package com.gabrielcostanovaes.main;
 
+import com.gabrielcostanovaes.http.ApiRequest;
+
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        ApiRequest apiRequest = new ApiRequest();
+
+        System.out.println("Digite o nome da comida: ");
+        String input = sc.nextLine();
+        apiRequest.getReceipt(input);
+
+    }
 }
